2b497d63d6b985ebf8762754c9c41b7268c975dc
b-bianca <you@example.com>
2021-03-04 22:04:50 -0300

    criação screenPage

--- lamusic-front/src/app/App.js
+++ lamusic-front/src/app/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import React from "react"
 import Router from "../routes/Router"
 import Header from '../components/Header'
@@ -8,8 +8,8 @@ export default function App() {
     return(
         <div>
             <BrowserRouter>
-              <Header/>
-              <Router/>
+               <Header/>
+               <Router/>
             </BrowserRouter>
         </div>
     )
